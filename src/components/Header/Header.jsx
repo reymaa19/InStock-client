f2b876e931f357d69eed85a88e9c802fb05f38a9
F.L <fang@FLs-MacBook-Pro.local>
2024-10-08 14:26:29 -0400

    add responsive styling for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,29 @@
 import "./Header.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo/logo.svg";
 
 function Header() {
   return (
     <header className="header">
-      <Link to="/warehouse">
-        <div className="header__logo">
-          <img src={logo} alt="InStock-logo" className="header__icon" />
-          <h1 className="header__title">INSTOCK</h1>
-        </div>
-      </Link>
+      <NavLink to="/warehouse">
+        <img src={logo} alt="InStock-logo" className="header__logo" />
+      </NavLink>
 
       <nav className="header__nav">
-        <Link to="/warehouse">
-          <h3 className="header_link">Warehouses</h3>
-        </Link>
-        <Link to="/inventory">
-          <h3 className="header_link">Inventory</h3>
-        </Link>
+        <NavLink
+          to="/warehouse"
+          className="header__link"
+          activeClassName="header__link--active"
+        >
+          <h3>Warehouses</h3>
+        </NavLink>
+        <NavLink
+          to="/inventory"
+          className="header__link"
+          activeClassName="header__link--active"
+        >
+          <h3>Inventory</h3>
+        </NavLink>
       </nav>
     </header>
   );
