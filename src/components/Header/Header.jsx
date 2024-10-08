eb0a89918b62fe5be208cdebe9b5baff8f9ec504
F.L <fang@FLs-MacBook-Pro.local>
2024-10-08 13:22:37 -0400

    add elements in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo/logo.svg";
 
 function Header() {
-  return <div>Header - Placeholder</div>;
+  return (
+    <header className="header">
+      <Link to="/warehouse">
+        <div className="header__logo">
+          <img src={logo} alt="InStock-logo" className="header__icon" />
+          <h1 className="header__title">INSTOCK</h1>
+        </div>
+      </Link>
+
+      <nav className="header__nav">
+        <Link to="/warehouse">
+          <h3 className="header_link">Warehouses</h3>
+        </Link>
+        <Link to="/inventory">
+          <h3 className="header_link">Inventory</h3>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
