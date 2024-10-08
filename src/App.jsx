50a54ef87c5737ca31ce09052d0381617029c2d6
Yvonne Itan <itan3040@mylaurier.ca>
2024-10-08 15:35:29 -0700

    Add the AddNewWareHouse Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
-
+import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,6 +13,8 @@ const App = () => {
         <Route path="/" element={<>default</>} />
         <Route path="/warehouse" element={<WarehousePage />} />
         <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/addnewwarehouse" element={<AddNewWarehouse />} />
+
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/components/AddNewWarehouse/AddNewWarehouse.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function AddNewWarehouse() {
+  return (
+    <div className="add-warehouse">
+      <h1 className="add-warehouse__title">Add New Warehouse</h1>
+      <form className="details">
+        <div className="details__address">
+            <h2 className="details__adress-title">Warehouse Details</h2>
+            <label htmlFor="warehouse">Warehouse Name</label>
+            <input type="text" id="warehouse" name="warehouse" placeholder="Warehouse Name" />
+            <label htmlFor="address">Street Address</label>
+            <input type="text" id="address" name="address" placeholder="Street Address" />
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city" placeholder="City" />
+            <label htmlFor="country">Country</label>
+            <input type="text" id="country" name="country" placeholder="Country" />
+        </div>
+
+        <div className="details__contacts">
+        <h2 className="details__adress-title">Contacts Details</h2>
+            <label htmlFor="contact-name">Contact Name</label>
+            <input type="text" id="contact-name" name="contact-name" placeholder="Contact Name" />
+            <label htmlFor="position">Position</label>
+            <input type="text" id="position" name="position" placeholder="Position" />
+            <label htmlFor="phone-number">Phone Number</label>
+            <input type="text" id="phone-number" name="phone-number" placeholder="Phone Number" />
+            <label htmlFor="Email">Email</label>ß
+            <input type="text" id="Email" name="Email" placeholder="Email" />
+        </div>
+
+        <button type="submit">Cancel</button>
+        <button type="submit">Submit</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default AddNewWarehouse;
+
+
