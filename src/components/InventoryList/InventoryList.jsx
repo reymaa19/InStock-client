aa56e31848fe5cdb0ec8065d3af3423cd70a3822
cassiek <ckumpula123@gmail.com>
2024-10-09 13:26:22 -0400

    set up InventoryList component

---
+++ src/components/InventoryList/InventoryList.jsx
@@ -0,0 +1,12 @@
+import "./InventoryList.scss";
+/// import hooks, assets, components...
+
+function InventoryList() {
+    return (
+        <div>
+            Placeholder!
+        </div>
+    );
+}
+
+export default InventoryList;
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./InventoryPage.scss";
+import Header from "../../components/Header/Header.jsx";
+import InventoryList from "../../components/InventoryList/InventoryList.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
 
 function InventoryPage() {
-  return <div>InventoryPage - Placeholder</div>;
+
+
+  return (
+    <div>
+      <Header />
+      <InventoryList />
+      <Footer />
+    </div>
+  );
 }
 
 export default InventoryPage;
