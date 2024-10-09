7aef1bc1275b68dc2f6209a580c4df1f07abd5ef
F.L <fang@FLs-MacBook-Pro.local>
2024-10-09 17:53:47 -0400

    finish table/desktop styling for editWarehouse

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -15,92 +15,94 @@ function EditWarehouse() {
         </div>
 
         <form className="details">
-          <div className="details__address">
-            <h2 className="details__adress-name">Warehouse Details</h2>
-            <label htmlFor="warehouse" className="details__address-label">
-              Warehouse Name
-            </label>
-            <input
-              type="text"
-              id="warehouse"
-              name="warehouse"
-              placeholder="Warehouse Name"
-              className="details__address-input"
-            />
-            <label htmlFor="address" className="details__address-name">
-              Street Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              placeholder="Street Address"
-              className="details__address-input"
-            />
-            <label htmlFor="city" className="details__address-label">
-              City
-            </label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              placeholder="City"
-              className="details__address-input"
-            />
-            <label htmlFor="country" className="details__address-label">
-              Country
-            </label>
-            <input
-              type="text"
-              id="country"
-              name="country"
-              placeholder="Country"
-              className="details__address-input"
-            />
-          </div>
+          <div className="details__tablet-container">
+            <div className="details__address">
+              <h2 className="details__adress-name">Warehouse Details</h2>
+              <label htmlFor="warehouse" className="details__address-label">
+                Warehouse Name
+              </label>
+              <input
+                type="text"
+                id="warehouse"
+                name="warehouse"
+                placeholder="Warehouse Name"
+                className="details__address-input"
+              />
+              <label htmlFor="address" className="details__address-name">
+                Street Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                placeholder="Street Address"
+                className="details__address-input"
+              />
+              <label htmlFor="city" className="details__address-label">
+                City
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                placeholder="City"
+                className="details__address-input"
+              />
+              <label htmlFor="country" className="details__address-label">
+                Country
+              </label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                placeholder="Country"
+                className="details__address-input"
+              />
+            </div>
 
-          <div className="details__contacts">
-            <h2 className="details__contacts-title">Contacts Details</h2>
-            <label htmlFor="contact-name" className="details__contacts-name">
-              Contact Name
-            </label>
-            <input
-              type="text"
-              id="contact-name"
-              name="contact-name"
-              placeholder="Contact Name"
-              className="details__contacts-input"
-            />
-            <label htmlFor="position" className="details__contacts-name">
-              Position
-            </label>
-            <input
-              type="text"
-              id="position"
-              name="position"
-              placeholder="Position"
-              className="details__contacts-input"
-            />
-            <label htmlFor="phone-number" className="details__contacts-name">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              id="phone-number"
-              name="phone-number"
-              placeholder="Phone Number"
-              className="details__contacts-input"
-            />
-            <label htmlFor="Email" className="details__contacts-name">
-              Email
-            </label>
-            <input
-              type="text"
-              id="Email"
-              name="Email"
-              placeholder="Email"
-              className="details__contacts-input"
-            />
+            <div className="details__contacts">
+              <h2 className="details__contacts-title">Contacts Details</h2>
+              <label htmlFor="contact-name" className="details__contacts-name">
+                Contact Name
+              </label>
+              <input
+                type="text"
+                id="contact-name"
+                name="contact-name"
+                placeholder="Contact Name"
+                className="details__contacts-input"
+              />
+              <label htmlFor="position" className="details__contacts-name">
+                Position
+              </label>
+              <input
+                type="text"
+                id="position"
+                name="position"
+                placeholder="Position"
+                className="details__contacts-input"
+              />
+              <label htmlFor="phone-number" className="details__contacts-name">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                id="phone-number"
+                name="phone-number"
+                placeholder="Phone Number"
+                className="details__contacts-input"
+              />
+              <label htmlFor="Email" className="details__contacts-name">
+                Email
+              </label>
+              <input
+                type="text"
+                id="Email"
+                name="Email"
+                placeholder="Email"
+                className="details__contacts-input"
+              />
+            </div>
           </div>
           <div className="details__button-container">
             <button
