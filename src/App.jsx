550684364be0b1309ac19b1ed5a66f038a6102b6
F.L <fang@FLs-MacBook-Pro.local>
2024-10-09 13:17:22 -0400

    add editwarehouse route in app

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,21 @@ import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import WarehouseInventoryPage from "./pages/WarehouseInventoryPage/WarehouseInventoryPage.jsx";
 import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
+import EditWarehouse from "./components/EditWarehouse/EditWarehouse.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="container"> 
-      <Header />
-      <Routes>
-        <Route path="/" element={<WarehousePage />} />
-        <Route path="/warehouse" element={<WarehousePage />} />
-        <Route path="/warehouse/:id" element={<WarehouseInventoryPage />} />
-        <Route path="/inventory" element={<InventoryPage />} />
-        <Route path="/addnewwarehouse" element={<AddNewWarehouse />} />
-      
-      </Routes>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<WarehousePage />} />
+          <Route path="/warehouse" element={<WarehousePage />} />
+          <Route path="/warehouse/:id" element={<WarehouseInventoryPage />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/addnewwarehouse" element={<AddNewWarehouse />} />
+          <Route path="/editwarehouse" element={<EditWarehouse />} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
