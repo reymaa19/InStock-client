b9b99aef0cc5800ae6d6241b21b5cdd8edf954ed
reymaa19 <rcmaala@gmail.com>
2024-10-09 15:27:48 -0500

    Create AddEditInventory component and finish styling mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
-import WarehouseInventoryPage from "./pages/WarehouseInventoryPage/WarehouseInventoryPage.jsx";
+import WarehouseInventoryPage from "./pages/WarehouseInventoryPage/WarehouseInventoryPage";
 import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
+import AddEditInventory from "./components/AddEditInventory/AddEditInventory";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="container"> 
-      <Header />
-      <Routes>
-        <Route path="/" element={<WarehousePage />} />
-        <Route path="/warehouse" element={<WarehousePage />} />
-        <Route path="/warehouse/:id" element={<WarehouseInventoryPage />} />
-        <Route path="/inventory" element={<InventoryPage />} />
-        <Route path="/addnewwarehouse" element={<AddNewWarehouse />} />
-      
-      </Routes>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<WarehousePage />} />
+          <Route path="/warehouse" element={<WarehousePage />} />
+          <Route path="/warehouse/:id" element={<WarehouseInventoryPage />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/addnewwarehouse" element={<AddNewWarehouse />} />
+          <Route path="/inventory/new" element={<AddEditInventory />} />
+          <Route path="/inventory/:id" element={<AddEditInventory />} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
