e52c2443eee856ce3ace70a05543cc47c95c1d3f
Anoshka Jhaveri <anoshkaujhaveri@gmail.com>
2024-10-09 02:04:53 -0700

    added warehouse name and made responsive

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,28 +1,42 @@
 import { Link } from "react-router-dom";
 import "./WarehouseDetails.scss";
+import arrow from "../../assets/images/icons/navigation/arrow_back-24px.svg";
+import editHeader from "../../assets/images/icons/action/edit-24px.svg";
 
 function WarehouseDetails(details) {
   const warehouse = details.details;
   return (
     <div className="details">
-      <div className="details__wrapper">
-        <div className="details__container details__container--left">
-          <h4 className="details__header">WAREHOUSE ADDRESS:</h4>
-          <p className="details__value">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
+      <div className="nav">
+        <img className="nav__arrow" src={arrow} alt="Return to Warehouses" />
+        <h1 className="nav__name">{warehouse.warehouse_name}</h1>
+        <div className="nav__end">
+          <div className="nav__color">
+            <img className="nav__edit" src={editHeader} alt="Edit Warehouse" />
+            <p className="nav__edit-text">Edit</p>
+          </div>
         </div>
       </div>
-
-      <div className="details__wrapper details__wrapper--right">
-        <div className="details__container">
-          <h4 className="details__header">CONTACT NAME: </h4>
-          <p className="details__value">{warehouse.contact_name}</p>
-          <p className="details__value">{warehouse.contact_position}</p>
+      <div className="details__warehouse">
+        <div className="details__wrapper">
+          <div className="details__container details__container--left">
+            <h4 className="details__header">WAREHOUSE ADDRESS:</h4>
+            <p className="details__value">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
+          </div>
         </div>
 
-        <div className="details__container">
-          <h4 className="details__header">CONTACT INFORMATION: </h4>
-          <p className="details__value">{warehouse.contact_phone}</p>
-          <p className="details__value">{warehouse.contact_email}</p>
+        <div className="details__wrapper details__wrapper--right">
+          <div className="details__container">
+            <h4 className="details__header">CONTACT NAME: </h4>
+            <p className="details__value">{warehouse.contact_name}</p>
+            <p className="details__value">{warehouse.contact_position}</p>
+          </div>
+
+          <div className="details__container">
+            <h4 className="details__header">CONTACT INFORMATION: </h4>
+            <p className="details__value">{warehouse.contact_phone}</p>
+            <p className="details__value">{warehouse.contact_email}</p>
+          </div>
         </div>
       </div>
     </div>
