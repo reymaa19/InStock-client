eea8eb3428031dfa84a8ea5b2e4d09aaccc43c0a
cassiek <ckumpula123@gmail.com>
2024-10-10 05:24:26 -0400

    create InventoryListItem to populate InventoryPage

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,17 +1,69 @@
 import React from "react";
 import "./InventoryPage.scss";
-import Header from "../../components/Header/Header.jsx";
-import InventoryList from "../../components/InventoryList/InventoryList.jsx";
-import Footer from "../../components/Footer/Footer.jsx";
+import InventoryListItem from "../../components/InventoryList/InventoryListItem.jsx";
+import { useState, useEffect } from "react";
+import { getInventory } from "../../services/inventory-api.js";
+import unfoldMore from "../../assets/images/icons/navigation/sort-24px.svg";
 
 function InventoryPage() {
-
-
-  return (
+  const [search, setSearch] = useState("");
+  const [inventory, setInventory] = useState([]);
+  useEffect(() => {
+    const fetchInventory = async () => {
+      const response = await getInventory();
+      console.log(response.data);
+      setInventory(response.data);
+    };
+    fetchInventory();
+  }, []);
+  
+  return ( 
     <div>
-      <Header />
-      <InventoryList />
-      <Footer />
+      <main className="main"> 
+        <section className="warehouse"> 
+          <div className="warehouse__container warehouse__container--search"> 
+            <h1 className="warehouse__page-header">Inventory</h1> 
+            <div className="warehouse__wrapper"> 
+              <input 
+              className="warehouse__search"
+              type="text"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              />
+              <button className="warehouse__cta-button"> 
+                + Add New Item 
+              </button> 
+           </div> 
+          </div> 
+          <div className="warehouse__container--headers"> 
+           <h4 className="warehouse__header"> 
+               INVENTORY ITEM{" "} 
+               <img className="warehouse__sort" src={unfoldMore} alt="sort" /> 
+             </h4> 
+             <h4 className="warehouse__header"> 
+               CATEGORY{" "} 
+               <img className="warehouse__sort" src={unfoldMore} alt="sort" /> 
+             </h4> 
+             <h4 className="warehouse__header warehouse__header--contact-name"> 
+               STATUS{" "} 
+               <img className="warehouse__sort" src={unfoldMore} alt="sort" /> 
+             </h4> 
+             <h4 className="warehouse__header"> 
+               QTY{" "} 
+               <img className="warehouse__sort" src={unfoldMore} alt="sort" /> 
+             </h4> 
+             <h4 className="warehouse__header"> 
+               WAREHOUSE{" "} 
+               <img className="warehouse__sort" src={unfoldMore} alt="sort" /> 
+             </h4> 
+             <h4 className="warehouse__header">ACTIONS</h4> 
+           </div> 
+            {inventory.map((invItem) => (  
+            <InventoryListItem key={invItem.id} item={invItem} /> 
+         ))} 
+        </section> 
+      </main> 
     </div>
   );
 }
