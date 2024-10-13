aa3a285518c448aa576990855081c9761f354c2f
reymaa19 <rcmaala@gmail.com>
2024-10-12 19:25:34 -0500

    Rename class and add name attribute to avoid console errors in DashboardPage

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
-import "./DashboardPage.scss";
-import WarehouseList from "../../components/WarehouseList/WarehouseList";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import InventoryList from "../../components/InventoryList/InventoryList";
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import "./DashboardPage.scss";
 
 const DashboardPage = () => {
   const location = useLocation();
@@ -27,20 +26,21 @@ const DashboardPage = () => {
   return (
     <main className="main">
       <section className="home">
-        <div className="home__container home__container--search">
-          <h1 className="home__page-header">
+        <div className="dashboard__container dashboard__container--search">
+          <h1 className="dashboard__page-header">
             {isInventory ? "Inventory" : "Warehouses"}
           </h1>
-          <div className="home__wrapper">
+          <div className="dashboard__wrapper">
             <input
-              className="home__search"
+              id="dashboard-search"
+              className="dashboard__search"
               type="text"
               placeholder="Search..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
             <Link
-              className="home__cta-button"
+              className="dashboard__cta-button"
               to={`/${isInventory ? "inventory" : "warehouse"}/add`}
             >
               + Add New {isInventory ? "inventory" : "warehouse"}
